2f7cac706a0bf911fe97ce1b862ffd93d19f1d4c
dashainCodes <dashain2.07@gmail.com>
2024-05-30 18:02:25 +0545

    hi

---
+++ dist/assets/pictures.js
@@ -0,0 +1,31 @@
+import logo from "./logo.png";
+import logo2 from "./logo2.png";
+import login from "./Login Logo.png";
+import playstore from "./google-play-btn 1.png";
+import applestore from "./app-store-btn 1.png";
+import cardHolder from "./card holder.png"
+import card from "./card.png"
+import rect from "./Rectangle 44.png"
+import profile from "./profile.png"
+import mobile_holder from "./mobile_holder.png"
+
+import ios_and_ic from "./ios_and_ic.png"
+import mob_ic from "./mob_ic.png"
+import con_ic from "./con_ic.png"
+
+import fb_logo from "./fb_logo.png"
+import google_logo from "./google_logo.png"
+import phone_logo from "./phone_logo.png"
+
+import fb_ic from "./fb_ic.png";
+import yt_ic from "./yt_ic.png";
+import tt_ic from "./tt_ic.png";
+import ig_ic from "./ig_ic.png";
+import li_ic from "./li_ic.png";
+
+import dummy from "./dummy.png"
+
+export {logo,logo2,login,playstore,applestore,cardHolder,card,
+    rect,profile,fb_ic,ig_ic,li_ic,yt_ic,tt_ic,ios_and_ic,mob_ic,con_ic,mobile_holder,dummy,
+fb_logo,google_logo,phone_logo
+}
